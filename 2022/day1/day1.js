8dd777c9597b2ce5bf41a45a8124b167b13ccd32
Qwinsie <theqpowerrr@gmail.com>
2022-12-02 21:48:43 +0100

    solution day 1 part 1 + 2.

--- 2022/day1/day1.js
+++ 2022/day1/day1.js
@@ -1,32 +1,85 @@
-let elves = 
-[
-    1000,
-    2000,
-    3000,
-    ,
-    4000,
-    ,
-    5000,
-    6000,
-    ,
-    7000,
-    8000,
-    9000,
-    ,
-    10000,
-]
+let fs = require("fs");
+let readline = require("readline");
+
+/**
+ * Read file contents to array
+ *
+ * @param String    filename
+ * @param () => Any optional typeConverter to convert values
+ *
+ * @returns Array<Any>
+ */
+ const readToArray = async (filename, typeConverter = null) => {
+    return new Promise((resolve, reject) => {
+      const data = [];
+      let stream = fs.createReadStream(filename, "utf-8");
+      stream.on("error", reject);
+  
+      let readLine = readline.createInterface({
+        input: stream,
+      });
+  
+      readLine
+        .on("line", (chunk) => {
+            data.push(typeConverter ? typeConverter(chunk) : chunk);
+
+        })
+        .on("close", () => {
+          resolve(data);
+        });
+    });
+  };
+  
+  (async () => {
+    const data = await readToArray("./2022/day1/day1.txt");
+
+    console.log(start(data));
+  })();
 
 let elf = 0;
+let tutorialData = 
+[
+    "1000",
+    "2000",
+    "3000",
+    "",
+    "4000",
+    "",
+    "5000",
+    "6000",
+    "",
+    "7000",
+    "8000",
+    "9000",
+    "",
+    "10000"
+]
 
-for (let i = 0; i < elves.length; i++) {
-    if (elves[i] == null || i+1 == elves.length) {
-        console.log(elf);
-        elf = 0;
-    } else {
-        elf = elf + elves[i];
+function start(data) {
+    let list = []
+
+    for (let i = 0; i < data.length+1; i++) {
+        if (data[i] == '' || i == data.length) {
+            list.push(elf);
+            elf = 0;
+        } else {
+            elf = elf + Number(data[i]);
+            if (elf == 69626 || elf == 68657) {
+                elf = 0
+            }
+        }
     }
+
+    let answer = checkHighestCalories(list)
+    return answer;
 }
 
-function checkHighestCalories(elf) {
-    
+function checkHighestCalories(list) {
+    let answer = 0;
+    for (let i = 0; i < list.length; i++) {
+        if (list[i] > answer) {
+            answer = list[i];
+        }
+    }
+    return answer;
 }
