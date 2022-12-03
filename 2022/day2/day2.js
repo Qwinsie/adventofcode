f083ccb980586e18c0c302bee24e1d9cf044e35c
Qwinsie <theqpowerrr@gmail.com>
2022-12-03 16:30:58 +0100

    day2 part 1 + 2 done.

--- 2022/day2/day2.js
+++ 2022/day2/day2.js
@@ -1,8 +1,61 @@
 import { readToArray}  from "../fileReader.js";
 
 (async () => {
-    const commands = await readToArray("2022/day2/day2.txt");
-    console.log(commands);
+    const data = await readToArray("strategy.txt");
+    console.log(`Answer: ${start(data)}`);
 })();
 
+function start(data) {
+    let points = 0
+    for (let i = 0; i < data.length; i++) {
+        let match = data[i].toString()
+        console.log(points);
+        points = points + checkWinner(match.slice(0, 1), match.slice(2))
+    }
+    return points
+}
 
+function checkWinner(opponent, me) {
+    switch (me) {
+        case "X":
+            me = opponent == "A" ? "Z"
+            : opponent == "B" ? "X"
+            : opponent == "C" ? "Y"
+            : console.log("error");
+            break;
+        case "Y":
+            me = opponent == "A" ? "X"
+            : opponent == "B" ? "Y"
+            : opponent == "C" ? "Z"
+            : console.log("error");;
+            break;
+        case "Z":
+            me = opponent == "A" ? "Y"
+            : opponent == "B" ? "Z"
+            : opponent == "C" ? "X"
+            : console.log("error");;
+            break;
+        default:
+            break;
+    }
+
+   switch (opponent) {
+    case "A":
+        return me == "Y" ? 8 
+        : me == "X" ? 4
+        : me == "Z" ? 3
+        : console.log("error");;
+    case "B":
+        return me == "Y" ? 5 
+        : me == "X" ? 1
+        : me == "Z" ? 9
+        : console.log("error");;
+    case "C":
+        return me == "Y" ? 2
+        : me == "X" ? 7
+        : me == "Z" ? 6
+        : console.log("error");;
+    default:
+        break;
+   }
+}
