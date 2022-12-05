4ed1d31a58c11263a219860c93d663ad7c4911ca
Qwinsie <theqpowerrr@gmail.com>
2022-12-05 22:29:10 +0100

    wip day5.

--- 2022/day5/day5.js
+++ 2022/day5/day5.js
@@ -2,5 +2,20 @@ import { readToArray}  from "../fileReader.js";
 
 (async () => {
     const data = await readToArray("input.txt");
-    console.log(`Part 1 Answer: ${partone(data)} | Part 2 Answer: ${parttwo(data)}`);
+    console.log(`Part 1 Answer: ${partOne(data)} | Part 2 Answer: ${partTwo(data)}`);
 })();
+
+function partOne(data) {
+    data = data
+    let procedure = data.slice(data.indexOf("")+1)
+    console.log(procedure);
+
+    let answer = 0
+    return answer
+}
+
+
+function partTwo(data) {
+    let answer = 0
+    return answer
+}
